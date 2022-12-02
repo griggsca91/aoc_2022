5a8cb539f9768e221ffda4188b48dd97056201e9
Christian Griggs <griggsca91@gmail.com>
2022-12-01 21:32:10 -0500

    renaming and parsing input once

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,25 +1,44 @@
 use std::num::ParseIntError;
 
 fn main() {
+
+    let contents = std::fs::read_to_string("input.txt").unwrap();
+
     if std::env::args().len() == 1 {
-        day_one();
+        day_one(&contents);
     }
 
     for arg in std::env::args() {
         match arg.as_str() {
-            "--day_one" => day_one(),
-            "--day_two" => day_two(),
+            "--day_one" => day_one(&contents),
+            "--day_two" => day_two(&contents),
             _ => {},
         }
     }
 }
 
-fn day_two() {
+fn day_one(input: &String) {
+    let mut tmp_calorie_sum = 0;
+    let mut max_calories = 0;
+    for line in input.split("\n") {
+        match line.parse::<i32>() {
+            Ok(cal) => tmp_calorie_sum += cal,
+            Err(_) => {
+                if max_calories < tmp_calorie_sum {
+                    max_calories = tmp_calorie_sum;
+                }
+                tmp_calorie_sum = 0;
+            }
+        };
+    }
+    println!("Day 1: max calories {}", max_calories);
+}
+
+fn day_two(input: &String) {
     let mut thicc_elves = vec![0, 0, 0];
-    let contents = std::fs::read_to_string("input_1.txt").unwrap();
 
     let mut tmp_cal_sum = 0;
-    for line in contents.split("\n") {
+    for line in input.split("\n") {
         match line.parse::<i32>() {
             Ok(cal) => tmp_cal_sum += cal,
             Err(_) => {
@@ -35,25 +54,6 @@ fn day_two() {
         };
     }
 
-    println!("{:?}", thicc_elves);
-    println!("max calories {:?}", thicc_elves.iter().sum::<i32>());
+    println!("Day 2: max calories {:?}", thicc_elves.iter().sum::<i32>());
 }
 
-fn day_one() {
-    let contents = std::fs::read_to_string("input_1.txt").unwrap();
-
-    let mut tmp_calorie_sum = 0;
-    let mut max_calories = 0;
-    for line in contents.split("\n") {
-        match line.parse::<i32>() {
-            Ok(cal) => tmp_calorie_sum += cal,
-            Err(_) => {
-                if max_calories < tmp_calorie_sum {
-                    max_calories = tmp_calorie_sum;
-                }
-                tmp_calorie_sum = 0;
-            }
-        };
-    }
-    println!("max calories {}", max_calories);
-}
