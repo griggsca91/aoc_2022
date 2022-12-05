eb09f000b954a677c835a2021fa603bf713397ec
Christian Griggs <griggsca91@gmail.com>
2022-12-05 18:00:22 -0500

    asdf

--- day_2/target/debug/day_2.d
+++
@@ -1,1 +0,0 @@
-/Users/chris/projects/aoc_2022/day_2/target/debug/day_2: /Users/chris/projects/aoc_2022/day_2/src/main.rs
--- day_2/target/debug/deps/day_2-ceeb2408848b2049.d
+++
@@ -1,5 +0,0 @@
-/Users/chris/projects/aoc_2022/day_2/target/debug/deps/day_2-ceeb2408848b2049: src/main.rs
-
-/Users/chris/projects/aoc_2022/day_2/target/debug/deps/day_2-ceeb2408848b2049.d: src/main.rs
-
-src/main.rs:
